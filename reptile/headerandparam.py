2b73a3f17b9a7f666ef94a42b5dfc9f23bd7dd92
864932125@qq.com <864932125@qq.com>
2018-10-09 18:02:29 +0800

    new

--- reptile/headerandparam.py
+++ reptile/headerandparam.py
@@ -9,16 +9,11 @@ import time
 import re
 import requests, json
 
-import requests
-import re
-import csv
-import time
-
-
+# 添加headers
+# 添加get，pos参数
 class downloader(object):
     def __init__(self):
         self.server = 'https://www.jianshu.com/u/5b4978fe1f2a'
-        self.allsongid = []  # 存放
 
     def get_download_url(self):
         url = self.server
@@ -37,25 +32,14 @@ class downloader(object):
         # pst请求
         # html = requests.post(url, headers=headers, params=data).text
         # 获取网页内容
-        # print(html)
+        print(html)
         # 获取网页元素
         soup = BeautifulSoup(html, 'lxml')
         # 根据条件筛选元素
         soup1 = soup.find('div', class_='col-xs-16 main')
         texs = soup1.find_all('a', class_="title")
         for tex in texs:
-            self.allsongid.append(tex.text)
             print(tex.text)
-        print(self.allsongid)
-        self.save_infor(self.allsongid)
-
-    def save_infor(one_page_film):
-        print(one_page_film)
-
-     # with open('name.csv', 'a', newline='', errors='ignore') as f:
-     #    csv_file = csv.writer(f)
-     #    for one in one_page_film:
-     #        csv_file.writerow(one)
 
 
 if __name__ == "__main__":
