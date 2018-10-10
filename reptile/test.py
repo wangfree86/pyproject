d7c1083ec2fd37818cd263dde4524d89f606b6d7
864932125@qq.com <864932125@qq.com>
2018-10-10 15:38:51 +0800

    反爬虫，已经获取简书数据的demo

--- reptile/test.py
+++ reptile/test.py
@@ -19,17 +19,18 @@ class downloader(object):
     def __init__(self):
         self.server = 'https://www.jianshu.com/u/5b4978fe1f2a'
         self.allsongid = []  # 存放
+        self.page = 1
 
     def get_download_url(self):
         url = self.server
-        print(url)
+        # print(url)
 
         # 添加headers
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0'}
         # 添加get，pos参数
         data = {'order_by': 'shared_at',
-                'page': '10',
+                'page': self.page,
                 }
         # get请求
         # https://www.jianshu.com/u/5b4978fe1f2a?order_by=shared_at&page=2
@@ -45,19 +46,32 @@ class downloader(object):
         texs = soup1.find_all('a', class_="title")
         for tex in texs:
             self.allsongid.append(tex.text)
-            print(tex.text)
-        print(self.allsongid)
+            # print(tex.text)
+        # print(self.allsongid)
         self.save_infor(self.allsongid)
+        time.sleep(1)
+        self.page = self.page + 1
+        self.get_download_url()
 
-    def save_infor(one_page_film):
+    def save_infor(self, one_page_film):
+        # print("-------")
         print(one_page_film)
 
-     # with open('name.csv', 'a', newline='', errors='ignore') as f:
-     #    csv_file = csv.writer(f)
-     #    for one in one_page_film:
-     #        csv_file.writerow(one)
+        with open('name1.csv', 'a', newline='', errors='ignore') as f:
+            csv_file = csv.writer(f)
+            for one in one_page_film:
+                csv_file.writerow([one, ""])
 
 
 if __name__ == "__main__":
-    dl = downloader()
-    dl.get_download_url()
+    obj = None
+    if obj is None:
+        print('a is not empty')
+    else:
+        print('qqqq')
+# dl = downloader()
+# dl.page+= 11
+# if 1>0:
+#     print(dl.page)
+# dl.get_download_url()
+# save_infor(dl.allsongid)
