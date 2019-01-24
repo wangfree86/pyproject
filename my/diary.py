4094c33230b9aef4438671dab3dc4658bd028318
864932125@qq.com <864932125@qq.com>
2019-01-24 09:22:13 +0800

    日记头信息生成代码修改

--- my/diary.py
+++ my/diary.py
@@ -8,6 +8,9 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
 import pyperclip
+import time
+
+import requests
 
 
 # world操作
@@ -44,23 +47,64 @@ def getweather():
 
     url = base_url
 
-    html = urlopen(url).read().decode('utf-8')
+    # 添加headers
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0'}
+    html = requests.get(url).text
+    # html = urlopen(url).read().decode('utf-8')
     soup = BeautifulSoup(html, features='lxml')
     # print(html)
     # 天气
     wea = soup.find('dd', class_='weather')
-    kongqi=soup.find('dd', class_='kongqi')
-    kongqistr=(kongqi.h5.text+kongqi.h6.text).replace('空气质量：', '')
+    kongqi = soup.find('dd', class_='kongqi')
+    kongqistr = (kongqi.h5.text + kongqi.h6.text).replace('空气质量：', '')
 
     weather = "天气：" + wea.span.text + "    " + kongqistr
     return weather
 
 
+# 获取当前的时间，格式化我默认的格式2019年1月15日 星期二
+def getNowDay():
+    year = time.strftime("%Y年", time.localtime())
+    m = time.strftime("%m月", time.localtime())
+    if m[0] == '0':
+        m = m.replace('0', '')
+    d = time.strftime("%d日", time.localtime())
+    if d[0] == '0':
+        d = d.replace('0', '')
+    week = time.strftime("%w", time.localtime())
+    week = num_to_week(int(week))
+    nstr = year + m + d + " " + week
+    return nstr
+    # nstr=time.strftime("%Y年%m月%d日  %x", time.localtime())
+    # allday = calendar.monthrange(datetime.datetime.now().year, datetime.datetime.now().month)[1]
+    # newday = datetime.datetime.now().day
+    # finallyday = str(allday - newday)
+    # return finallyday
+
+
+def num_to_week(num):
+    numbers = {
+        0: "星期日",
+        1: "星期一",
+        2: "星期二",
+        3: "星期三",
+        4: "星期四",
+        5: "星期五",
+        6: "星期六",
+    }
+    return numbers.get(num, None)
+
+
+import locale
+
 if __name__ == '__main__':
-    docx_file_name = 'C:/Users/XH/Desktop/模板.docx'
-    docx_file_name1 = 'C:/Users/XH/Desktop/模板1.docx'
-    # 获取文件对象
-    file = docx.Document(docx_file_name)
+
+    locale.setlocale(locale.LC_CTYPE, 'chinese')
+    # docx_file_name = 'C:/Users/XH/Desktop/模板.docx'
+    # docx_file_name1 = 'C:/Users/XH/Desktop/模板1.docx'
+    # # 获取文件对象
+    # file = docx.Document(docx_file_name)
     # 三个参数: 旧的字符串, 新的字符串, 文件对象
     weather = getweather()
 
@@ -70,9 +114,10 @@ if __name__ == '__main__':
     # replace_text('本月余额', day, file)
     # file.save(docx_file_name1)
 
-    str = weather + '\n' + day
+    str =getNowDay() + '\n' + weather + '\n' + day
     pyperclip.copy(str)
-
-    print("成功"+str)
+    getNowDay()
+    # 2019年1月15日 星期二
+    print("成功" + str)
     # print(docx_file_name1, "替换成功"+str)
 # main()
